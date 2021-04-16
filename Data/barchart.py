e084ff6665c4b3f2a2f894ac8dafc459b22e0681
Sarikayaelif <sarikayaelif1412@gmail.com>
2021-04-16 16:02:16 -0400

    creating bar chart with python

---
+++ Data/barchart.py
@@ -0,0 +1,17 @@
+# Number of the gold medals:220
+# Number of the silver medals:96
+# Number of the bronze medals:35
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+labels = ['Gold', 'Silver', 'Bronze']
+values = [220,96,35]
+ax.bar(labels,values)
+ax.get_children()[0].set_color('Gold')
+ax.get_children()[1].set_color('Silver') 
+ax.get_children()[2].set_color((0.804,0.498,0.196))
+for i, v in enumerate(values):
+    ax.text(-0.05 + i, v + i, str(v), color='blue', fontweight='bold', fontsize = 18)
+plt.show()
