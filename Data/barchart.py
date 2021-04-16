188c9efd1ee347c4c3dbc4810b2ea41ea1b04bc8
Sarikayaelif <sarikayaelif1412@gmail.com>
2021-04-16 17:16:52 -0400

    Last changes on html file

--- Data/barchart.py
+++ Data/barchart.py
@@ -12,6 +12,17 @@ ax.bar(labels,values)
 ax.get_children()[0].set_color('Gold')
 ax.get_children()[1].set_color('Silver') 
 ax.get_children()[2].set_color((0.804,0.498,0.196))
+
+plt.ylabel("number of medals")
+
+
+#label on the bottom of the chart
+plt.xlabel("Gold", "Silver", "Bronze")
+
+#add a title to the chart
+plt.title("Number of medals in gold, silver and bronze", pad="20")
+
+
 for i, v in enumerate(values):
     ax.text(-0.05 + i, v + i, str(v), color='blue', fontweight='bold', fontsize = 18)
 plt.show()
